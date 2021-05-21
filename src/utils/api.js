164be1efc3a8de844114b02e63e08e4444c771f4
Zaino <hello@darrin.dev>
2021-05-20 21:18:33 -0700

    add randomuser.me api

---
+++ src/utils/api.js
@@ -0,0 +1,12 @@
+// ✅ todo import axios 
+import axios from "axios";
+// ✅ todo use https://randomuser.me/ for easy random user results
+const url = 'https://randomuser.me/api/?results=12';
+
+
+export default {
+    ApiSearch: function () {
+        return axios.get(url)
+
+    }
+}
