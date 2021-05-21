8b55bf37dcc4d2be1d45450d1957db1db15b73ed
Zaino <hello@darrin.dev>
2021-05-20 21:44:31 -0700

    start components

---
+++ src/components/Search/index.js
@@ -0,0 +1,9 @@
+// ✅ todo import react 
+// ✅ todo import component styles 
+import React from "react";
+import "./style.css";
+
+//todo define the component
+//todo render the search form 
+
+export default Search;
---
+++ src/components/Table/index.js
@@ -0,0 +1,15 @@
+// ✅ todo import react 
+// ✅ todo import the API call
+// ✅ todo import component styles 
+import React from "react";
+import API from "../../utils/api.js"
+//? to do import Search from "../Search"
+import "./style.css";
+
+//todo set up state 
+//todo get the Api call of random users
+//todo handle the search input 
+//todo sort table by name 
+//todo render the table 
+
+export default Table;
